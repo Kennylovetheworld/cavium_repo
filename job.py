46382b1803769a6e8d3d115e1586fecf18fab051
Kennylovetheworld <944545895@qq.com>
2020-01-09 22:53:56 -0500

    init

--- job.py
+++ job.py
@@ -1,6 +1,5 @@
 from pyspark import SparkConf, SparkContext
 import sys
-import json
 
 # This script takes two arguments, an input and output
 if len(sys.argv) != 3:
@@ -11,20 +10,33 @@ input = sys.argv[1]
 output = sys.argv[2]
 
 # Set up the configuration and job context
-conf = SparkConf().setAppName('kennyjob')
+conf = SparkConf().setAppName('AnnualWordLength')
 sc = SparkContext(conf=conf)
 
+
 # Read in the dataset and immediately transform all the lines in arrays
-rawdata = sc.textFile(input)
-data = rawdata.map(lambda line: json.loads(line.decode('utf-8')))
+data = sc.textFile(input).map(lambda line: line.split('\t'))
 
 # Create the 'length' dataset as mentioned above. This is done using the next two variables, and the 'length' dataset ends up in 'yearlyLength'.
-out = data.map(
-    lambda arr: (arr['subreddit'], arr['created_utc'])
+yearlyLengthAll = data.map(
+    lambda arr: (int(arr[1]), float(len(arr[0])) * float(arr[2]))
+)
+yearlyLength = yearlyLengthAll.reduceByKey(lambda a, b: a + b)
+
+# Create the 'words' dataset as mentioned above.
+yearlyCount = data.map(
+    lambda arr: (int(arr[1]), float(arr[2]))
+).reduceByKey(
+    lambda a, b: a + b
+)
+
+# Create the 'average_length' dataset as mentioned above.
+yearlyAvg = yearlyLength.join(yearlyCount).map(
+    lambda tup: (tup[0], tup[1][0] / tup[1][1])
 )
 
 # Save the results in the specified output directory.
-data.saveAsTextFile(output)
+yearlyAvg.saveAsTextFile(output)
 
 # Finally, let Spark know that the job is done.
 sc.stop()
